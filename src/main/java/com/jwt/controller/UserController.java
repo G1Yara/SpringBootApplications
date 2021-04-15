7c576ea53e86ce01090cc71e67de576dbd4be551
JeevanYara <jeevan.yara@costrategix.com>
2021-04-15 22:51:40 +0530

    Added swagger

--- src/main/java/com/jwt/controller/UserController.java
+++ src/main/java/com/jwt/controller/UserController.java
@@ -3,6 +3,7 @@ package com.jwt.controller;
 import com.jwt.model.UserDto;
 import com.jwt.security.JwtAuthenticationEntryPoint;
 import com.jwt.services.UserService;
+import io.swagger.annotations.Api;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,12 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
+@Api(tags = "User")
 public class UserController {
     private static final Logger logger = Logger.getLogger(JwtAuthenticationEntryPoint.class);
     @Autowired
     private UserService userService;
 
-       @PostMapping("/v1/ register")
+       @PostMapping("/v1/register")
     public ResponseEntity<?> authenticateUser(@RequestBody UserDto userDto) {
         return ResponseEntity.ok(userService.createUser(userDto));
     }
